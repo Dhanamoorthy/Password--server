d1543f12f97211d497eee44dc07b61b4c58a023e
Dhanamoorthy <dhanasekar9540@gmail.com>
2024-07-02 12:02:52 +0530

    frontend link attached

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ import nodemailer from 'nodemailer';
 const app = express();
 dotenv.config();
 const corsOptions = {
-  origin: 'http://localhost:3000',
+  origin: 'https://dulcet-custard-6a5a47.netlify.app/',
   credentials: true,  // <-- this is important to allow credentials (cookies, authorization headers)
 };
 
